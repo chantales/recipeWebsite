568c5974426b212d1f7293fe1ddee2f5fd70e39d
waxfighter <119152805+waxfighter@users.noreply.github.com>
2025-07-15 21:35:03 +1200

    15/7

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDaFwUkn50NhSRt3FgS68lidSap4NqyPxk",
+  authDomain: "listofrecipes-d88b8.firebaseapp.com",
+  databaseURL: "https://listofrecipes-d88b8-default-rtdb.firebaseio.com",
+  projectId: "listofrecipes-d88b8",
+  storageBucket: "listofrecipes-d88b8.firebasestorage.app",
+  messagingSenderId: "147673777707",
+  appId: "1:147673777707:web:9c9a187722377fec470c6e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+import {getDatabase, set, get, update, remove, ref, child}
+from "https://www.gstatic.com/firebasejs/11.10.0/firebase-database.js";
+
+const db = getDatabase();
+
+
 function searchBarFunction() {
       // Declare variables
       var input, filter, ul, li, a, i, txtValue;
